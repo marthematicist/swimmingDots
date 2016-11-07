a77ea29374d3b738b9b29b1f0812e4dc20cd1010
marthematicist <hite.32@gmail.com>
2016-11-07 12:36:09 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -46,8 +46,8 @@ function draw() {
     for( n = 0 ; n < N ; n++ ) {
       fill(0);
       //ellipse( 0.5*dx + m*dx , 0.5*dy + n*dy , dx , dy );
-      ax = m/M*4*PI + 2*PI*t/T;
-      ay = n/N*4*PI + 2*PI*t/T;
+      ax = m/M*6*PI + 2*PI*t/T;
+      ay = n/N*6*PI + 2*PI*t/T;
       fill(0);
       ellipse( 0.5*dx + m*dx + (1-a)*sin(ax)*dx , 0.5*dy + n*dy + (1-a)*cos(ay)*dy , a*dx , a*dy );
     }
